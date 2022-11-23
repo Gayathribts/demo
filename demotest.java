a9290f725cc95ca454b8aaa0cc1cb1a5588aafc8
Gayathribts <115998841+Gayathribts@users.noreply.github.com>
2022-11-23 13:04:13 +0530

    Update demotest.java

--- demotest.java
+++ demotest.java
@@ -2,6 +2,6 @@ package .com.aer;
 public class demotest
 {
   public static void main(string args[])
-        system.out.println("i love bts");
-        system.out.println("i am indian");
+        System.out.println("i love bts");
+        System.out.println("i am indian");
 }
